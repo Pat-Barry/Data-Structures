715ab818a10a84dfd5e1e8e883464b00016563a9
Patrick Barry <pwbarry1@gmail.com>
2021-10-30 20:23:50 -0400

    added Sorting-Searching directory with binary search and merge sort files. Also added methods to find_minimum.py

---
+++ Lists/find_minimum.py
@@ -0,0 +1,46 @@
+# Method 1: sorting the list. Time complexity = O(nlogn)
+def merge_sort(lst):
+    if len(lst) > 1:
+        mid = len(lst) // 2
+        left = lst[:mid]
+        right = lst[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = 0
+        j = 0
+
+        k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                lst[k] = left[i]
+                i += 1
+            else:
+                lst[k] = right[j]
+                j += 1
+
+            k += 1
+        
+        while i < len(left):
+            lst[k] = left[i]
+            i += 1
+            k += 1
+        
+        while j < len(right):
+            lst[k] = right[j]
+            j += 1
+            k += 1
+        
+
+def find_minimum1(lst):
+    if len(lst) == 0:
+        return None
+
+    merge_sort(lst)
+    return lst[0]
+
+lst = [2,-100,5,6,-7,8,9,3,2]
+
+print(find_minimum1(lst))
